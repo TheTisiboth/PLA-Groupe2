9d570772cd62980d49002e48dcb6649efaa11322
leni <lenigauffier@gmail.com>
2018-05-30 17:19:25 +0200

    1

--- src/utils/MapParser.java
+++ src/utils/MapParser.java
@@ -9,6 +9,16 @@ public class MapParser {
 
 	private static final String MAPPATH = "assets/maps/";
 	
+	private static final int[][] colors = {
+										{255,0,0}, //red = ennemi
+										{0,255,0}, //green = item
+										{0,0,255}, //blue = 
+										{255,255,0}, //yellow = porte
+										{255,0,255}, //pink = boss
+										{0,255,255}, //cyan	= 
+										{0,0,0} //black = mur
+	};
+	
 	public static void get_map(String mapname){
 
 		BufferedReader br = null;
@@ -19,10 +29,24 @@ public class MapParser {
 			fr = new FileReader(MAPPATH + mapname + ".ppm");
 			br = new BufferedReader(fr);
 
-			String sCurrentLine;
+			String line;
 
-			while ((sCurrentLine = br.readLine()) != null) {
-				System.out.println(sCurrentLine);
+			br.readLine();
+			line = br.readLine();
+			int[] dim = {Integer.parseInt(line.split(" ")[0]),Integer.parseInt(line.split(" ")[1])};
+			br.readLine();
+			
+			int compt = 0;
+			
+			int color_list[dim[0]][dim[1]];
+			
+			int rgb[3];
+			
+			while ((line = br.readLine()) != null) {
+						
+				System.out.println(line);
+				compt = ++compt == 3 ? 0 : compt;
+				
 			}
 
 		} catch (IOException e) {
@@ -42,11 +66,10 @@ public class MapParser {
 			} catch (IOException ex) {
 
 				ex.printStackTrace();
-
 			}
-
 		}
-
 	}
+	
+
 
 }
