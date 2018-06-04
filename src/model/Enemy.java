3fbca8d8c2b68744504429f1694baff0e6aed2d8
leni <lenigauffier@gmail.com>
2018-06-04 10:40:57 +0200

    Revert "Revert "layers added""
    This reverts commit fa80fe1d628f725cedb35c956863cf9216438e83.

--- src/model/Enemy.java
+++ src/model/Enemy.java
@@ -4,6 +4,9 @@ public class Enemy extends Entity {
 
 	static String m_spriteFile = "assets/sprites/wall.png";
 
+	static int m_layer = 1;
+
+
 	public Enemy(Model model, int x, int y, String spriteFile) {
 		super(model, x, y, spriteFile);
 	}
