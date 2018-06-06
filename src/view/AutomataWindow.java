071c6bd9960d0258b354f16d3ea018a07c17b30e
EvaBardou <laocia38@gmail.com>
2018-06-06 11:52:28 +0200

    Add buttons with Sprites on the UI

---
+++ src/view/AutomataWindow.java
@@ -0,0 +1,14 @@
+package view;
+
+import javax.swing.JFrame;
+
+public class AutomataWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public AutomataWindow() {
+		this.setLocationRelativeTo(null);
+		
+		this.setVisible(true);
+	}
+}
