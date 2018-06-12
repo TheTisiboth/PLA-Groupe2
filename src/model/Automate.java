1c0f6fa21fd1cf62ad5099db6007beac7ad5ae06
Joachim Fontfreyde <joachim.ffde@gmail.com>
2018-06-12 11:24:44 +0200

    fix eclipse setup

---
+++ src/model/Automate.java
@@ -0,0 +1,14 @@
+package model;
+
+import j.*;
+
+public class Automate extends Entity{
+	J_Automaton comportement;
+	
+	public Automate(Model model, int posX, int posY, boolean moveable, String filename, Tile t) {
+		super(model, posX, posY, moveable, filename, t);
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
