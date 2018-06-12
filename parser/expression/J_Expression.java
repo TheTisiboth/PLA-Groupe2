d15f19c80da7c587b67504398c634ebb042e858d
Gautier Pignonblanc Yann <y.gautier136@laposte.net>
2018-06-12 16:28:10 +0200

    conflict

--- parser/expression/J_Expression.java
+++ parser/expression/J_Expression.java
@@ -7,20 +7,13 @@ public abstract class J_Expression {
 	public String toString() {
 		return "Expression";
 	}
-	
-<<<<<<< HEAD
+
 	public boolean eval(Entity e) {
 		System.out.print("Eval pas definie pour cette expression");
 		return false;
 	}
 	
 	public boolean eval() {
-=======
-	//TODO abstract
-	public  boolean eval() {
->>>>>>> e114b222fbedd15ca743e222716a5109a46316e4
-		System.out.print("Eval pas definie pour cette expression");
-		return false;
-	}
+
 	
 }
