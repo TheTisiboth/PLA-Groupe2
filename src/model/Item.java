d6133643752cb5ff62deb0c2e7b67b4e3102861c
Joachim Fontfreyde <joachim.ffde@gmail.com>
2018-06-13 15:32:59 +0200

    merge

--- src/model/Item.java
+++ src/model/Item.java
@@ -11,7 +11,7 @@ public class Item extends Entity {
 	// static int m_layer = 0;
 	// static double m_startingSpeed = Options.velocities.get("item"); 
 
-	enum ItemType{
+	public enum ItemType{
 		WEAPON,
 		CONSUMABLE
 	}
@@ -20,12 +20,13 @@ public class Item extends Entity {
 	String m_filepath;
 	int m_damage;
 	
-	public Item(Model model, int x, int y, String spriteFile, Tile t, int damage) {
+	public Item(Model model, int x, int y, String spriteFile, Tile t, int damage, ItemType item) {
 		super(model, x, y, false, spriteFile, t, Teams.Pickable);
 		m_layer = Options.layers.get("character");
 		m_type = ItemType.WEAPON;
 		m_filepath = spriteFile;
 		m_damage = damage;
+		m_type = item;
 	}
 	
 	public ItemInv getItemInv(AliveEntity entity) {
