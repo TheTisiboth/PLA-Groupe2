41b7569918644074a42b2b59f50d5ae23beb7140
Gautier Pignonblanc Yann <y.gautier136@laposte.net>
2018-06-13 11:44:15 +0200

    cell ok

--- parser/expression/FunCall/Condition/Cell.java
+++ parser/expression/FunCall/Condition/Cell.java
@@ -3,10 +3,12 @@ package expression.FunCall.Condition;
 import java.util.List;
 import java.util.ListIterator;
 
+import controller.Options;
 import j.J_Parameters;
 import main.Directions;
 import main.Teams;
 import model.Entity;
+import model.Model;
 
 public class Cell extends J_Condition {
 
@@ -16,17 +18,40 @@ public class Cell extends J_Condition {
 	}
 
 	public boolean eval(Entity e) {
+		Directions dir = e.RelativeToRealDir(parameters.get(0).toDirection());
 		Teams team = parameters.get(1).toEntite();
-		Directions d = parameters.get(0).toDirection();
-		List<Entity> l = e.checkTile(e.RelativeToRealDir(d));
-		ListIterator<Entity> iter = l.listIterator();
-		while(iter.hasNext()) {
-			Entity ent = iter.next();
-			if(team.equals(ent.getTeam())) {
-				return true;
-			}
+		
+		Model model = e.m_model;
+		
+		int x = e.getTile().getCaseX();
+		int y = e.getTile().getCaseY();
+		switch(dir) {
+			case LEFT:
+				x--;
+			case RIGHT:
+				x++;
+			case UP:
+				y--;
+			case DOWN:
+				y++;
 		}
-		return false;
+		switch(team) {
+			case Void:
+				for(int i = 3; i >= 0; i--) {
+					if(model.getRoom().getTile(x, y).getEntityOnLayer(i) != null) { 
+							return false;
+					}
+				}
+				return true;
+			default :
+				for(int i = 3; i >= 0; i--) {
+					if(model.getRoom().getTile(x, y).getEntityOnLayer(i) != null) {
+						if(model.getRoom().getTile(x, y).getEntityOnLayer(i).getTeam() == team) 
+							return true;
+					}
+				}
+				return false ;
+		}
 	}
 	
 }
